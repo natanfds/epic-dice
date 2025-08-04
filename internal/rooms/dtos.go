0ac33eedf5a1164452d2d632d59c0caf22befa5a
natanfds <natanfernandesdossantos@zohomail.com>
2025-08-04 09:24:26 -0300

    fix: removendo virgula nas definições das dtos

--- internal/rooms/dtos.go
+++ internal/rooms/dtos.go
@@ -1,21 +1,21 @@
 package rooms
 
 type MessageDTO struct {
-	Content string `json:"content", binding:"required"`
+	Content string `json:"content" validate:"required"`
 }
 
 type Arg struct {
-	Content string `json:"content", binding:"required"`
-	Param   string `json:"param", binding:"required"`
+	Content string `json:"content" validate:"required"`
+	Param   string `json:"param" validate:"required"`
 }
 
 type CommandDTO struct {
-	Type string `json:"type", binding:"required"`
-	Args []Arg  `json:"args", binding:"required"`
+	Type string `json:"type" validate:"required"`
+	Args []Arg  `json:"args" validate:"required"`
 }
 
 type MessageResponseDTO struct {
-	Content string `json:"content", binding:"required"`
+	Content string `json:"content" validate:"required"`
 }
 
 type CommandResponseDTO struct {
